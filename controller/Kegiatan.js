a36f46fd79f31b109e468e7dc519b5159e323295
rifkiramadan095 <rifkiramadan095@gmail.com>
2021-04-22 15:37:50 +0700

    Pertemuan_8

--- controller/Kegiatan.js
+++ controller/Kegiatan.js
@@ -1,4 +1,5 @@
 const kegiatanModel = require('../model/Kegiatan')
+const objectId = require('mongoose').Types.ObjectId
 
 exports.create = (data) =>
 new Promise((resolve, reject) => {
@@ -10,5 +11,76 @@ new Promise((resolve, reject) => {
     })).catch(() => reject({
         status: false,
         pesan: 'GAGAL INPUT KEGIATAN'
+    }))
+})
+
+exports.showALLData = () =>
+new Promise ((resolve, reject) => {
+    kegiatanModel.find()
+    .then(result => {
+        resolve({
+            status: true,
+            pesan : 'Berhasil menampilkan data',
+            data : result
+        })
+    }).catch((err) => {
+        console.log(err)
+        reject ({
+            status : false,
+            pesan: 'gagal menampilkan data',
+            data : []
+        })
+    })
+
+})
+
+exports.showALLDataById = (id) =>
+new Promise((resolve, reject) => {
+    try  {
+        kegiatanModel.findOne({
+            _id: objectId(id)
+        })
+            .then(result => {
+                resolve({
+                    status: true,
+                    pesan : 'Berhasil menampilkan data',
+                    data : result
+                })
+        }).catch((err) => {
+            console.log(err)
+            reject ({
+                status : false,
+                pesan: 'gagal menampilkan data',
+                data : null
+            })
+        })
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+exports.update = (id, data) =>
+new Promise ((resolve, reject) => {
+    kegiatanModel.updateOne({
+        _id : objectId(id)
+    },data).then(() => resolve({
+        status : true,
+        pesan : 'Berhasil Mengubah Data'
+    })).catch(() => reject ({
+        status : false,
+        pesan : 'Gagal Mengubah Data'
+    }))
+})
+
+exports.delete = (id) =>
+new Promise ((resolve, reject) => {
+    kegiatanModel.deleteOne({
+        _id: objectId (id)
+    }).then(() => resolve({
+        status : true,
+        pesan : 'Berhasil Menghapus Data'
+    })).catch(() => ({
+        status : false,
+        pesan: 'Gagal Menghapus Data'
     }))
 })
--- routes/Kegiatan.js
+++ routes/Kegiatan.js
@@ -7,4 +7,26 @@ router.post('/insert', (req, res) => {
     .catch(err => res.json(err))
 })
 
+router.get('/getall', (req, res) => {
+    kegiatanController.showALLData()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+router.get('/getbyid/:id', (req, res) => {
+    kegiatanController.showALLDataById(req.params.id)
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+router.put('/update/:id', (req, res) => {
+    kegiatanController.update(req.params.id, req.body)
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+router.delete('/delete/:id', (req, res) => {
+    kegiatanController.delete(req.params.id)
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
 module.exports = router
